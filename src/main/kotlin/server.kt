f297a8abf8dcd6d95533c83c161939e862e35a4f
Josua Frank <frank.josua@gmail.com>
2022-05-24 16:52:00 +0200

    Added CORS

--- src/main/kotlin/server.kt
+++ src/main/kotlin/server.kt
@@ -1,16 +1,23 @@
 import api.v1.cart.add
 import api.v1.progress.getProgress
 import api.v1.progress.setProgress
+import io.ktor.http.*
 import io.ktor.serialization.kotlinx.json.*
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import io.ktor.server.plugins.contentnegotiation.*
+import io.ktor.server.plugins.cors.routing.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
 
 fun main() {
     embeddedServer(Netty, port = 8080) {
+        install(CORS) {
+            anyHost()
+            allowHeader(HttpHeaders.ContentType)
+            allowHeader(HttpHeaders.Host)
+        }
         install(ContentNegotiation) {
             json(Json {
                 isLenient = true
